e6d3ab706dda08668b8aa34f97a01dc7b917f4e3
Jessica Amy <jessicaannetteamy@gmail.com>
2024-12-02 19:28:31 +1030

    modules

---
+++ main.py
@@ -0,0 +1,11 @@
+#import my_module
+
+#from my_module import greeting
+
+import modules.my_module as spam
+
+print(dir(spam))
+
+#print(my_module.person)
+
+#print(greeting)
---
+++ modules/my_module.py
@@ -0,0 +1,11 @@
+greeting = 'Hello World'
+
+nums = [10, 20, 30, 40, 520]
+
+person = {
+    "name": "Jesso",
+    "age": 27
+}
+
+def foo(person):
+    print(f'{person.get('name')} is {person.get('age')} years old.')
